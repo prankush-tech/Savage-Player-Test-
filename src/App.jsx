40e870df285f11a4e626c40e7569e56d33531056
Prankush Giri <prankushgiri@gmail.com>
2024-01-06 00:30:54 +0530

    Controls Added✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import CanvasComponent from './components/CanvasComponent'
+import Controls from './components/Controls'
 
 const App = () => {
   return (
     <React.Fragment>
+      <Controls/>
       <CanvasComponent/>
     </React.Fragment>
   )
---
+++ src/components/Controls.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Controls = () => {
+  return (
+    <React.Fragment>
+        <div className="containerControls">
+            <img src="/Controls.png" alt="Control-Image" />
+        </div>
+    </React.Fragment>
+  )
+}
+
+export default Controls
